b39d0037c69d387d27ec4e8ed719484c54a7b3d6
PolAndrew <andrew_polukhtovych@yahoo.com>
2016-03-19 13:41:41 +0200

    country

--- src/objects/Coutry.java
+++ src/objects/Coutry.java
@@ -3,5 +3,20 @@ package objects;
 import java.util.ArrayList;
 
 public class Coutry {
-	ArrayList<City> Country= new ArrayList<>(30);
+	
+	 static ArrayList<City> Country= new ArrayList<>(10);
+	
+	
+	static{
+		Country.add(new City("Lvov"));
+		Country.add(new City("Kiev"));
+		Country.add(new City("Lutsk"));
+		Country.add(new City("Kovel"));
+		Country.add(new City("Rovno"));
+		Country.add(new City("Odessa"));
+		Country.add(new City("Donetsk"));
+		Country.add(new City("Mariypol"));
+		Country.add(new City("Ryasne"));
+		Country.add(new City("Lukov"));
+	}
 }
